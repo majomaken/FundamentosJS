1e708931b58a656add50e0ee6e7b40c2904bb476
majomaken <miguel.jimenez@h3s.com.co>
2019-05-20 14:37:33 -0500

    first class

---
+++ clase1.js
@@ -0,0 +1,11 @@
+// var nombre
+//Semicolon is your choose
+var nombre = 'Miguel'
+var apellido = 'Jimenez'
+var edad = 28
+
+edad = '28 años'
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
