b682e6decfa877f43b41ac6d49e7e5fa4327a288
majomaken <majo.maken@gmail.com>
2019-05-22 21:42:05 -0500

    learning functions in class 4

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'miguel' , edad = 21
+
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+imprimirEdad(nombre, edad)
+imprimirEdad('Nina', 8)
+imprimirEdad('Yuli', 16)
+imprimirEdad(25, 'Carlos')
